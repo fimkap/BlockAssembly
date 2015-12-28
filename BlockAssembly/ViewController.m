e24305813dfedf7eb2672c55d6956bbcf26a17c9
Efim Polevoi <efim.polevoi@cellebrite.com>
2015-12-28 18:55:47 +0200

    Some tests.

--- BlockAssembly/ViewController.m
+++ BlockAssembly/ViewController.m
@@ -9,19 +9,49 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic,retain) NSDictionary *dictionary;
+//@property (nonatomic,copy) void(^MyBlock)(void);
 @end
 
 @implementation ViewController
 
+@synthesize dictionary/*, MyBlock*/;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    /*
+    [self setMyBlock:^{
+        self.dictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"obj", @"key", nil];
+        }];
+        */
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
+    //__block ViewController *selfRef = self;
+    //dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0), ^{
+    //    selfRef.dictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"obj", @"key", nil];
+    //});
+    //dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0), self.MyBlock);
+    dispatch_time_t dispatchTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC));
+    dispatch_after(dispatchTime, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        self.dictionary = [NSDictionary dictionaryWithObjectsAndKeys:@"obj", @"key", nil];
+    });
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)onTouchDidReceived:(__unused id)sender {
+    NSLog(@"touched");
 }
 
 @end
